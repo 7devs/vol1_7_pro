ddf61d972c917795fdbf047670aa731da045f3ba
Seven Yu <dofyyu@gmail.com>
2016-10-14 12:53:37 +0800

    update menu and add some pages

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var app = require('express')(),
+var express = require('express'),
+    app = express(),
+    // 引入系统提供的 path 包
+    path = require('path'),
     // 引入配置文件
     conf = require('./lib/config'),
     // 引入微信 API 功能包
@@ -9,6 +12,17 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+// 设置模版目录
+app.set('views', path.join(__dirname, 'lib/views'));
+// 设置模版引擎
+app.set('view engine', 'ejs');
+
+// 静态文件目录
+app.use(express.static(path.join(__dirname, 'static'), {
+    index: false,
+    maxAge: '7 days'
+}));
+
 // 初始化微信 API 功能包
 wechat(conf.wechat);
 
@@ -17,6 +31,7 @@ wechat(conf.wechat);
 wechat.createMenu(require('./lib/menu.json'));
 
 app.use('/wxapi', require('./lib/routers/wxapi.js'));
+app.use('/pages', require('./lib/routers/pages.js'));
 
 app.listen(8017, function(err) {
     console.log('listenning at 8017...');
